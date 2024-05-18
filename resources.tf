c9ddc30fad9fba7903616c29f656815e83e7548f
Nevil Jeevan <nevil.jeevan@saint-gobain.com>
2024-05-18 18:51:53 +0530

    add new tags, subnet

--- resources.tf
+++ resources.tf
@@ -17,7 +17,8 @@ data "aws_availability_zones" "available" {}
 ##################################################################################
 locals {
   common_tags = {
-    env = "development"
+    env      = "development"
+    billcode = "001"
   }
 }
 
