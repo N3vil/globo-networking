5168fb2bc4add4b5b08f07f3c0a21e05d453c32d
Nevil Jeevan <nevil.jeevan@saint-gobain.com>
2024-05-18 18:44:08 +0530

    fix configuration

--- resources.tf
+++ resources.tf
@@ -40,3 +40,25 @@ module "main" {
 
   tags = local.common_tags
 }
+
+resource "aws_security_group" "ingress" {
+  description = "Security group with no ingress rule"
+  egress = [
+    {
+      cidr_blocks = [
+        "0.0.0.0/0",
+      ]
+      description      = null
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    },
+  ]
+  ingress = []
+  name    = "no-ingress-sg"
+  vpc_id  = module.main.vpc_id
+}
